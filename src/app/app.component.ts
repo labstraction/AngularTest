05c4c02c508866a134a02e4fc3b4550164cc4433
labstraction <45163263+labstraction@users.noreply.github.com>
2022-03-30 10:01:39 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent  implements OnInit{
+
+  title = 'AngularTest';
+
+  user = {name: "andrea", loginDate: new Date(), results:[100, 1002, 1]}
+
+  user2?: any;
+
+  ngOnInit(): void {
+    //window.sessionStorage.setItem("ciao", "mondo");
+    window.localStorage.setItem("pippo", JSON.stringify(this.user));
+    window.localStorage.setItem("pluto", JSON.stringify(this.user));
+
+
+    if (localStorage.getItem("pluto")) {
+      this.user2 = JSON.parse(localStorage.getItem("pluto") as any); 
+    }
+    
+    const key = "name"
+    const value = "simone"
+    const date = new Date()
+    date.setFullYear(date.getFullYear() + 1);
+
+    const cookie = key + '=' + value + ';expires=' + date.toUTCString() + ';path=/;SameSite=Lax';
+
+    console.log(cookie);
+
+    document.cookie = cookie;
+
+
+    const cookie2 = 'pippo' + '=' + 'pluto' + ';expires=' + date.toUTCString() + ';path=/;SameSite=Lax';
+
+    document.cookie = cookie2;
+
+    console.log(document.cookie);
+    
+  }
+}
